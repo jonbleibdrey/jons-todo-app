a71266ec82c740bb16babc0dae178fd829318cf7
jonathan bleibdrey <jonbleibdrey@aol.com>
2021-05-11 16:49:39 -0400

    added update

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,21 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Todo = ({ todoList, setTodoList, todo, text }) => {
+  const [update, setUpdate] = useState(text);
+  const [onEdit, setOnEdit] = useState(false);
 
-    const handleDelete = () => {
-        setTodoList(todoList.filter((elem) => elem.id !== todo.id))
-    }
+  const handleDelete = () => {
+    setTodoList(todoList.filter((elem) => elem.id !== todo.id));
+  };
+
+  const handleUpdate = (e) => {
+    e.preventDefault()
+    setTodoList(
+        todoList.map((item) => {
+          if (item.id === todo.id) {
+            return {
+              ...item,
+              text: update,
+            };
+          }
+          return item;
+        })
+      );
+      setOnEdit(false);
+  };
+
+  const inputTextHandler = (e) => {
+    setUpdate(e.target.value)
+  };
 
   return (
-    <ul>
-      <li>
-        <div>
-          <h1>Todos</h1>
-          {text}
-          <button onClick={handleDelete}>delete</button>
-        </div>
-      </li>
-    </ul>
+    <>
+      {onEdit ? (
+        <form onSubmit={handleUpdate}>
+          <input type="text" onChange={inputTextHandler} value={update}/>
+          <button type="submit"> update change</button>
+        </form>
+      ) : (
+        <ul style={{ margin: "5rem" }}>
+          <li>
+            <h1>Todos</h1>
+            {text}
+            <button onClick={handleDelete}>delete</button>
+            <button onClick={() => setOnEdit(true)}>edit</button>
+          </li>
+        </ul>
+      )}
+    </>
   );
 };
 
